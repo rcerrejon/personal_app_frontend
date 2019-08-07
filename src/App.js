b56d2d996467ff5e151af0b0a91e5e24827addb2
Vadim Golenko <udachin.vadim@gmail.com>
2019-08-08 01:41:35 +0300

    Home page get avatar, github & greeting text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import style from './App.scss';
 import Routes from "./Routes";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Router, Route, Link } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee, faArrowRight, faArrowLeft, faEnvelopeOpenText, faTimes, faLanguage, faWindowClose, faLight } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCoffee, faArrowRight, faArrowLeft, faEnvelopeOpenText, faTimes, faLanguage, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import { fab, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Provider } from 'react-redux';
-import { combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
-import { createStore, renderDevTools } from './utils/devTools';
 import * as reducers from './reducers';
 
 import Console from './components/Console';
@@ -17,16 +17,16 @@ import Header from './components/Header';
 import NavbarMain from './components/NavbarMain';
 
 
-library.add(faCheckSquare, faCoffee, faArrowRight, faArrowLeft, faEnvelopeOpenText, faTimes, faLanguage, faWindowClose )
+library.add(faCheckSquare, faCoffee, faArrowRight, faArrowLeft, faEnvelopeOpenText, faTimes, faLanguage, faWindowClose, faGithub, fab )
 
 const history = createBrowserHistory();
 const reducer = combineReducers(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 )
 
 function App() {
