59dbdda76a99f5576900fb76792acc5da12eb058
Vadim Golenko <udachin.vadim@gmail.com>
2019-09-16 23:04:36 +0300

    Release

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,13 @@ COPY package-lock.json package-lock.json
 RUN apk add --no-cache python build-base
 RUN npm install
 RUN npm install -g node-gyp
+RUN apt update && apt install -y curl
+RUN curl -sL https://deb.nodesource.com/setup_8.x | bash -
+RUN apt update && apt install -y nodejs
 #RUN apk del build-base
 COPY . /app
 RUN npm run build
 
 EXPOSE 80
+CMD ["npm", "start"]
 ENTRYPOINT npm run start
--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!./co
 library.add(faCheckSquare, faCoffee, faArrowRight, faArrowLeft, faEnvelopeOpenText, faTimes, faLanguage, faWindowClose, faGithub, fab, faFile, faFolder, faExternalLinkAlt, faGitlab, faGoogle, faBehance, faBitcoin, faPaypal, faCcVisa, faChrome, faCodepen, faDiscord, faDigitalOcean, faDocker, faDribbble, faFacebookSquare, faFirefox, faFlipboard, faFreeCodeCamp, faGit, faGitAlt, faGooglePlusG, faInstagram, faItunes, faItunesNote, faJava, faJsSquare, faNodeJs, faNode, faKickstarter, faLinkedin, faLinux, faMedium, faMicrosoft, faNpm, faOdnoklassnikiSquare, faOpera, faPatreon, faCcPaypal, faPeriscope, faPhp, faPinterest, faPlaystation, faPython, faReact, faReadme, faReddit, faSafari, faSass, faLess, faSistrix, faSketch, faSkype, faSlackHash, faSnapchatSquare, faSoundcloud, faStackOverflow, faSteam, faTelegram, faTrello, faTwitch, faTwitter, faUbuntu, faVk, faWindows, faWikipediaW, faWordpress, faXbox, faYahoo, faYandex, faYandexInternational, faYarn, faYoutube )
 
 const reducer = combineReducers(reducers);
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = /*window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||*/ compose;
 const store = createStore(
   reducer,
   composeEnhancers(
